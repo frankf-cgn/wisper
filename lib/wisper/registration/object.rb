3e7278d22d539fe159d7b7fb04a7cfd7498392ba
Frank Fischer <fischer@intertech.de>
2014-04-17 12:14:53 +0200

    made Ruby 1.8.7 compatible

--- lib/wisper/registration/object.rb
+++ lib/wisper/registration/object.rb
@@ -13,7 +13,11 @@ module Wisper
     def broadcast(event, publisher, *args)
       method_to_call = map_event_to_method(event)
       if should_broadcast?(event) && listener.respond_to?(method_to_call) && publisher_in_scope?(publisher)
-        listener.public_send(method_to_call, *args)
+        if listener.respond_to? :public_send
+          listener.public_send(method_to_call, *args)
+        else
+          listener.send(method_to_call, *args)
+        end
       end
     end
 
